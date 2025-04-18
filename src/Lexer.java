a387a1540c5f0c455cdb20bce63fac35bc8be96d
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2025-04-17 23:12:34 -0400

    initial commit

---
+++ src/Lexer.java
@@ -0,0 +1,14 @@
+
+
+public class Lexer {
+    private final String input;
+    private int position;
+
+    public Lexer(String input){
+        this.input = input;
+    }
+
+
+
+
+}
---
+++ src/Token.java
@@ -0,0 +1,14 @@
+public class Token {
+    public final TokenType tokenType;
+    public final String value;
+
+    public Token(TokenType tokenType, String value){
+        this.tokenType = tokenType;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Token{" + "type=" + type + ", value='" + value + "'}";
+    }
+}
