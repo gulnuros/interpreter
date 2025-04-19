7adf15cb101d92ee4fd2ea3961b2ef343679d0aa
gulnurkaztai <93098410+gulnurkaztai@users.noreply.github.com>
2025-04-18 23:59:02 -0400

    start thinking about position

--- src/Lexer.java
+++ src/Lexer.java
@@ -6,6 +6,7 @@ public class Lexer {
 
     public Lexer(String input){
         this.input = input;
+        this.position = 0;
     }
 
     public Token nextToken(){
@@ -13,6 +14,32 @@ public class Lexer {
         while(position < input.length && Character.isWhiteSpace(input.charAt(position))){
             position++;
         }
+        char current = input.charAt(position);
+
+        if (Character.isDigit(current)) {
+            StringBuilder number = new StringBuilder();
+            while (position < input.length() && Character.isDigit(input.charAt(position))) {
+                number.append(input.charAt(position));
+                position++;
+            }
+            return new Token(TokenType.NUMBER, number.toString());
+        }
+
+        if (current == '+') {
+            position++;
+            return new Token(TokenType.PLUS, "+");
+        }
+
+        if (Character.isLetter(current)) {
+            StringBuilder identifier = new StringBuilder();
+            while (position < input.length() && Character.isLetterOrDigit(input.charAt(position))) {
+                identifier.append(input.charAt(position));
+                position++;
+            }
+            return new Token(TokenType.IDENTIFIER, identifier.toString());
+        }
+
+        throw new RuntimeException("Unexpected character: " + current);
 
 
     }
